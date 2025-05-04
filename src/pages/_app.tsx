a03e9dc94e928e3746366e6cfc6654ce53cf4cd8
roham <sajjadkarimi766@gmail.com>
2025-05-04 16:33:43 +0330

    add fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,30 @@ import "@/styles/globals.css";
 import "@/styles/icons.css";
 import type { AppProps } from "next/app";
 import {Layout} from "@/components";
+import {Lato, Quicksand} from "next/font/google";
 
+const quickSand = Quicksand({
+    subsets:['latin'],
+})
+const lato = Lato({
+    subsets:['latin'],
+    weight:['100','300'],
+    variable:'--font-lato'
+})
 
 export default function App({ Component, pageProps }: AppProps) {
+
   return(
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <>
+          <style jsx global>{`
+        html {
+            --font-lato: ${lato.style.fontFamily},sans-serif;
+          font-family: ${quickSand.style.fontFamily},sans-serif;
+        }
+      `}</style>
+          <Layout>
+              <Component {...pageProps} />
+          </Layout>
+      </>
   );
 }
