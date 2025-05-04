f8d2fa52cd48d1996b4b9dbcd51a085ad496aaf2
roham <sajjadkarimi766@gmail.com>
2025-05-04 16:34:27 +0330

    add some mock data

---
+++ src/mock/menu.ts
@@ -0,0 +1,30 @@
+export const mockMenu = [
+    {
+        title:"Hot Deals",
+        icon:"icon-flame",
+        link:"#",
+        // titleClassName:"text-heading6 lg:text-heading-sm xl:text-heading6",
+        // linkClassName:"flex flex-row gap-2 items-center",
+    },
+    {
+        title:"Home",
+        icon:"",
+        link:"#",
+        // titleClassName:"",
+        // linkClassName:"flex items-center gap-1",
+    },
+    {
+        title:"Food",
+        icon:"",
+        link:"#",
+        // titleClassName:"",
+        // linkClassName:"flex flex-row",
+    },
+    {
+        title:"vegetables",
+        icon:"",
+        link:"#",
+        // titleClassName:"",
+        // linkClassName:"flex flex-row",
+    },
+]
