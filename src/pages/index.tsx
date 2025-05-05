33f4a2f8ed2706f7e1baec7251074a835f3c3122
roham <sajjadkarimi766@gmail.com>
2025-05-05 11:36:36 +0330

    fix some bug in navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,9 +30,9 @@ export default function Home() {
                 <h2 className="text-heading3 text-blue-300">Popular Products</h2>
                 <div className="flex items-center gap-3">
                     <IconBox icon={"swiper-nav-left icon-angle-small-left cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/>
-                    <IconBox icon={"swiper-nav-right icon-angle-small-left cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/> </div>
+                    <IconBox icon={"swiper-nav-right icon-angle-small-right cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/> </div>
                 </div>
-            <SimpleProductSlider nextEl={".swiper-nav-right"} prevEl={".swiper-nav-left"} sliderData={popularProducts}/>
+            <SimpleProductSlider prevEl={".swiper-nav-right"} nextEl={".swiper-nav-left"} sliderData={popularProducts}/>
         </Section>
 
         <Section>
@@ -40,10 +40,10 @@ export default function Home() {
                 <h2 className="text-heading3 text-blue-300">Popular Fruits</h2>
                 <div className="flex items-center gap-3">
                     <IconBox icon={"swiper-nav-left2 icon-angle-small-left cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/>
-                    <IconBox icon={"swiper-nav-right2 icon-angle-small-left cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/>
+                    <IconBox icon={"swiper-nav-right2 icon-angle-small-right cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/>
                 </div>
             </div>
-            <SimpleProductSlider nextEl={".swiper-nav-right2"} prevEl={".swiper-nav-left2"} sliderData={popularFruits}/>
+            <SimpleProductSlider prevEl={".swiper-nav-right2"} nextEl={".swiper-nav-left2"} sliderData={popularFruits}/>
         </Section>
     </>
   );
