825a23f7e4f4e13199a4dbd2210b56262b36c380
roham <sajjadkarimi766@gmail.com>
2025-05-07 19:44:22 +0330

    implement popular fruit api

--- src/api/Product.ts
+++ src/api/Product.ts
@@ -6,11 +6,13 @@ interface Props {
     populate?: Array<"categories"|"thumbnail"|"gallery"> ;
     filters?:{
         is_popualr?: boolean;
+        is_popular_fruit?: boolean;
     }
 }
 
 interface Filters {
     is_popular?: { $eq: boolean };
+    is_popular_fruit?: { $eq: boolean };
 }
 
 export function getAllProducts({populate,filters}:Props):Promise<ApiResponseType<ProductType>> {
@@ -18,6 +20,9 @@ export function getAllProducts({populate,filters}:Props):Promise<ApiResponseType
     const customFilters:Filters = {};
     if (filters?.is_popualr){
         customFilters.is_popular = {$eq:filters.is_popualr};
+    }
+    if (filters?.is_popular_fruit){
+        customFilters.is_popular_fruit = {$eq:filters.is_popular_fruit};
     }
 
     return apiClient.get(`/products`,{
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ import {ProductType} from "@/types/api/Product";
 
 export default function Home() {
 
-   const {data : popularProductData} = useQuery<ApiResponseType<ProductType>>({queryKey:[getAllProducts.name],queryFn:()=>getAllProducts(
+   const {data : popularProductData} = useQuery<ApiResponseType<ProductType>>({queryKey:[getAllProducts.name,'popular_product'],queryFn:()=>getAllProducts(
            {
                populate:["categories","thumbnail"],
                filters:{
@@ -30,6 +30,14 @@ export default function Home() {
                }
            }
        )})
+    const {data : isPopularFruit} = useQuery<ApiResponseType<ProductType>>({queryKey:[getAllProducts.name,'popular_fruit'],queryFn:()=>getAllProducts(
+            {
+                populate:["categories","thumbnail"],
+                filters:{
+                    is_popular_fruit:true
+                }
+            }
+        )})
 
 
   return (
@@ -65,47 +73,50 @@ export default function Home() {
             }
         </Section>
 
-        {/*<Section>*/}
-        {/*    <div className="flex justify-between mb-[50px]">*/}
-        {/*        <h2 className="text-heading3 text-blue-300">Popular Fruits</h2>*/}
-        {/*        <div className="flex items-center gap-3">*/}
-        {/*            <IconBox icon={"swiper-nav-left2 icon-angle-small-left cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/>*/}
-        {/*            <IconBox icon={"swiper-nav-right2 icon-angle-small-right cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/>*/}
-        {/*        </div>*/}
-        {/*    </div>*/}
-        {/*    <SimpleProductSlider prevEl={".swiper-nav-right2"} nextEl={".swiper-nav-left2"} sliderData={popularFruits}/>*/}
-        {/*</Section>*/}
+        <Section>
+            <div className="flex justify-between mb-[50px]">
+                <h2 className="text-heading3 text-blue-300">Popular Fruits</h2>
+                <div className="flex items-center gap-3">
+                    <IconBox icon={"swiper-nav-left2 icon-angle-small-left cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/>
+                    <IconBox icon={"swiper-nav-right2 icon-angle-small-right cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/>
+                </div>
+            </div>
+            {
+                isPopularFruit &&
+                <SimpleProductSlider prevEl={".swiper-nav-right2"} nextEl={".swiper-nav-left2"} sliderData={isPopularFruit.data}/>
+            }
+        </Section>
 
-        {/*<Section>*/}
-        {/*    <div className="flex justify-between mb-[50px]">*/}
-        {/*        <h2 className="text-heading6 md:text-heading5 lg:text-heading4 xl:text-heading3 text-blue-300">Best Sellers</h2>*/}
-        {/*    </div>*/}
-        {/*    <div className="flex gap-[24px]">*/}
-        {/*        <div className="bg-[url('/assets/images/bg-leaf.png')] bg-no-repeat bg-bottom bg-[#3BB77E] rounded-[10px] shadow-[20px_20px_40px_0_rgba(24,24,24,0.07)] p-12 pt-[38px] self-stretch flex-col justify-between max-w-[370px] hidden xl:flex">*/}
-        {/*            <h3 className="text-heading2 text-blue-300">Bring nature into your home</h3>*/}
-        {/*            <Link href="#" className="mt-6 pl-[15px] pr-2.5 py-2 bg-yellow-100 hover:bg-green-200 rounded-[3px] cursor-pointer inline-flex max-w-max items-center gap-2.5">*/}
-        {/*                <div className="text-xsmall text-white">Shop now</div>*/}
-        {/*                <IconBox icon={"icon-arrow-small-right"} size={24}/>*/}
-        {/*            </Link>*/}
-        {/*        </div>*/}
-        {/*        <BestSellersSlider sliderData={bestSellers}/>*/}
-        {/*    </div>*/}
-        {/*</Section>*/}
+        <Section>
+            <div className="flex justify-between mb-[50px]">
+                <h2 className="text-heading6 md:text-heading5 lg:text-heading4 xl:text-heading3 text-blue-300">Best Sellers</h2>
+            </div>
+            <div className="flex gap-[24px]">
+                <div className="bg-[url('/assets/images/bg-leaf.png')] bg-no-repeat bg-bottom bg-[#3BB77E] rounded-[10px] shadow-[20px_20px_40px_0_rgba(24,24,24,0.07)] p-12 pt-[38px] self-stretch flex-col justify-between max-w-[370px] hidden xl:flex">
+                    <h3 className="text-heading2 text-blue-300">Bring nature into your home</h3>
+                    <Link href="#" className="mt-6 pl-[15px] pr-2.5 py-2 bg-yellow-100 hover:bg-green-200 rounded-[3px] cursor-pointer inline-flex max-w-max items-center gap-2.5">
+                        <div className="text-xsmall text-white">Shop now</div>
+                        <IconBox icon={"icon-arrow-small-right"} size={24}/>
+                    </Link>
+                </div>
+                <BestSellersSlider sliderData={bestSellers}/>
+            </div>
+        </Section>
 
-        {/*<Section>*/}
-        {/*    <div className="flex justify-between items-center mb-[50px]">*/}
-        {/*        <h2 className="text-heading6 md:text-heading5 lg:text-heading4 xl:text-heading3 text-blue-300">Deals Of*/}
-        {/*            The Days</h2>*/}
-        {/*        <Link className="flex items-center" href="#">All Deals*/}
-        {/*            <IconBox icon={"icon-angle-small-right text-[24px]"} size={24}/>*/}
-        {/*        </Link>*/}
-        {/*    </div>*/}
-        {/*    <DealsOfTheDays sliderData={dealsData}/>*/}
-        {/*</Section>*/}
+        <Section>
+            <div className="flex justify-between items-center mb-[50px]">
+                <h2 className="text-heading6 md:text-heading5 lg:text-heading4 xl:text-heading3 text-blue-300">Deals Of
+                    The Days</h2>
+                <Link className="flex items-center" href="#">All Deals
+                    <IconBox icon={"icon-angle-small-right text-[24px]"} size={24}/>
+                </Link>
+            </div>
+            <DealsOfTheDays sliderData={dealsData}/>
+        </Section>
 
-        {/*<Section>*/}
-        {/*    <GridSlider/>*/}
-        {/*</Section>*/}
+        <Section>
+            <GridSlider/>
+        </Section>
     </>
   );
 }
